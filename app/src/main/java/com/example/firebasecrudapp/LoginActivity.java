bd2cb3df46357720a4c5b86aeb863e094976f44d
Chinmay <chinmaymp007@gmail.com>
2023-02-09 13:27:14 +0530

    Added login and sign up interface with firebase auth, task is remaining

--- app/src/main/java/com/example/firebasecrudapp/LoginActivity.java
+++ app/src/main/java/com/example/firebasecrudapp/LoginActivity.java
@@ -2,13 +2,60 @@ package com.example.firebasecrudapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+    private TextInputEditText userName, passwd;
+    private Button btnLogin;
+    private ProgressBar progressBar;
+    private TextView registerText;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        userName = findViewById(R.id.idEdtUsername);
+        passwd = findViewById(R.id.idEdtPassword);
+        btnLogin = findViewById(R.id.loginbtn);
+        progressBar = findViewById(R.id.pbloading);
+        registerText = findViewById(R.id.logintv);
+        mAuth = FirebaseAuth.getInstance();
+
+        registerText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginActivity.this, RegistrationActivity.class);
+                startActivity(i);
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if(user!=null){
+            Intent i = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(i);
+            this.finish();
+        }
     }
 }
--- app/src/main/java/com/example/firebasecrudapp/RegistrationActivity.java
+++ app/src/main/java/com/example/firebasecrudapp/RegistrationActivity.java
@@ -1,14 +1,85 @@
 package com.example.firebasecrudapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    private TextInputEditText edtUser, edtPasswd, edtCnfPasswd;
+    private Button btnRegister;
+    private ProgressBar loadingPB;
+    private FirebaseAuth mAuth;
+    private TextView logintV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        edtUser = findViewById(R.id.idEdtUsername);
+        edtPasswd = findViewById(R.id.idEdtPassword);
+        edtCnfPasswd = findViewById(R.id.idEdtCnfPassword);
+        btnRegister = findViewById(R.id.registerbtn);
+        loadingPB = findViewById(R.id.pbloading);
+        logintV = findViewById(R.id.registertv);
+        mAuth = FirebaseAuth.getInstance();
+        logintV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(RegistrationActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadingPB.setVisibility(View.VISIBLE);
+                String username = edtUser.getText().toString();
+                String password = edtPasswd.getText().toString();
+                String cnfpasswd = edtCnfPasswd.getText().toString();
+                if(!password.equals(cnfpasswd)){
+                    Toast.makeText(RegistrationActivity.this, "Please Check the Password", Toast.LENGTH_SHORT);
+                }
+                else if(TextUtils.isEmpty(username) && TextUtils.isEmpty(password) && TextUtils.isEmpty(cnfpasswd)   ){
+                    Toast.makeText(RegistrationActivity.this, "Please add all credentials", Toast.LENGTH_SHORT);
+                }else{
+                    mAuth.createUserWithEmailAndPassword(username,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                loadingPB.setVisibility(View.GONE);
+                                Toast.makeText(RegistrationActivity.this, "Registration Completed", Toast.LENGTH_SHORT);
+                                Intent i = new Intent(RegistrationActivity.this, LoginActivity.class);
+                                startActivity(i);
+                                finish();
+                            }else{
+                                loadingPB.setVisibility(View.GONE);
+                                Toast.makeText(RegistrationActivity.this, "Failed to Register..!", Toast.LENGTH_SHORT);
+                            }
+                        }
+                    });
+
+                }
+            }
+        });
+
+
+
+
     }
 }
